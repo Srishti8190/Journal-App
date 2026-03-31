cca6dfcde38754c5136a6517c0f30133084c982c
Srishti8190 <srishti305yadav@gmail.com>
2026-03-31 14:48:58 +0530

    Update HealthCheck.java

--- src/main/java/com/srishti/Journal/App/Controller/HealthCheck.java
+++ src/main/java/com/srishti/Journal/App/Controller/HealthCheck.java
@@ -3,6 +3,8 @@ package com.srishti.Journal.App.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// this is only for health check like code is working or not
+
 @RestController
 public class HealthCheck {
 
